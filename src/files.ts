c5940752070042c60ba2438231aa1f44077d6ac8
razakj <jakub@startbit.com.au>
2022-10-19 11:00:30 +0800

    Added configurable content-disposition

--- src/files.ts
+++ src/files.ts
@@ -5,13 +5,14 @@ export interface ISendFileInfo {
     fileName: string;
     fileContent: any;
     contentType?: string;
+    inline?: boolean;
 }
 
 export async function registerFileOperations(api: Koa<any>) {
     api.use(async (ctx: ApiRouterContext<any>, next) => {
 
         ctx.sendFile = (options: ISendFileInfo) => {
-            ctx.set('Content-disposition', `attachment; filename=${options.fileName}`);
+            ctx.set('Content-disposition', `${options.inline ? "inline" : "attachment"}; filename=${options.fileName}`);
             if(options.contentType) ctx.set('Content-Type', `${options.contentType}`);
             ctx.status = 200;
             ctx.body = options.fileContent;
